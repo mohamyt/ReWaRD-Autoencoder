cb188eb3bace1b400caf0628be5fa135c97a5564
Mohammed Taha <105548037+mohamyt@users.noreply.github.com>
2024-06-08 17:27:56 +0300

    Update args.py

--- args.py
+++ args.py
@@ -7,23 +7,23 @@ def conf(args_list=None):
     parser.add_argument("--dataset", default="FractalDB-60", type = str, help="dataset name")
     # network settings
     parser.add_argument("--usenet", default="resnet50", type = str, help="use network")
-    parser.add_argument("--epochs", default=30, type = int, help="end epoch")
+    parser.add_argument("--epochs", default=90, type = int, help="end epoch")
     parser.add_argument("--numof_classes", default=1000, type = int, help="num of classes")
     # model hyper-parameters
-    parser.add_argument("--lr", default=0.001, type = float, help="initial learning rate")
+    parser.add_argument("--lr", default=0.01, type = float, help="initial learning rate")
     parser.add_argument("--momentum", default=0.9, type = float, help="momentum")
     parser.add_argument("--weight_decay", default=1e-4, type = float, help="weight decay")
     # etc
     parser.add_argument("--start-epoch", default=1, type = int, help="input batch size for training")
-    parser.add_argument("--batch_size", default=64, type = int, help="input batch size for training")
+    parser.add_argument("--batch_size", default=256, type = int, help="input batch size for training")
     parser.add_argument("--val-batch_size", default=256, type=int, help="input batch size for testing")
     parser.add_argument("--img_size", default=256, type = int, help="image size")
     parser.add_argument("--crop_size", default=256, type = int, help="crop size")
     parser.add_argument('--no_multigpu', default=False, action='store_true', help='If true, training is not performed.')
     parser.add_argument("--no-cuda", default=False, action="store_true", help="disables CUDA training")
     parser.add_argument("--gpu_id", default=-1, type = int, help="gpu id")
-    parser.add_argument("--num_workers", default=16, type = int, help="num of workers (data_loader)")
-    parser.add_argument("--save-interval", default=1, type = int, help="save every N epoch")
+    parser.add_argument("--num_workers", default=8, type = int, help="num of workers (data_loader)")
+    parser.add_argument("--save-interval", default=3, type = int, help="save every N epoch")
     parser.add_argument("--log-interval", default=100, type=int, help="how many batches to wait before logging training status")
     parser.add_argument("--seed", default=1, type=int, help="seed")
     
